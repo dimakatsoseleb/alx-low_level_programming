2419ba75a0c1f6efae8126473886d48925176f33
dimakatsoseleb <dimakatsoselebedi082422@gmail.com>
2022-09-16 07:43:45 -0700

    4. isalpha

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,17 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- *_islower - checks for a lowercase character
- *@c: letter being tested
- * Return: Always 0
+ *_islower - functions to check for a lowercase character
+ *@c: is the int that will use for the argument of the function 
+ * Return: 0
  */
-
 int _islower(int c)
 {
-
-	if (c >= 97 && c <= 122)
+	if (c >= 'a' && c <= 'z')
+         {
 		return (1);
+         }		
 	else
 		return (0);
 }
+^c
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,16 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- *_islower - checks for a lowercase character
- *@c: letter being tested
- * Return: Always 0
+ *_isalpha - function to checks if c is a letter,lowercase or uppercase
+ *@c: is the int that will use for the argument of the function
+ * Return: 0
  */
-
-int _islower(int c)
+int _isalpha(int c)
 {
 
-	if (c >= 97 && c <= 122)
+	if ((c >= 'a' && c <= 'z') || ( c >= 'A' && c <= 'Z'));
+	{
 		return (1);
+         }		
 	else
 		return (0);
 }
